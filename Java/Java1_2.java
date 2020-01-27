5c002514f4ce7ab4b4700838ce5efdbf8f8b2efa
liyang <YangKaw@126.com/KawYang@163.com>
2020-01-27 11:19:12 +0800

    delete

--- Java/Java1_2.java
+++
@@ -1,9 +0,0 @@
-public class Java1_2{
-    public static void main(String []args){
-        int x,y,sum;
-        x = 3;
-        y = 5;
-        sum = x + y;
-        System.out.println(sum);
-    }
-}
