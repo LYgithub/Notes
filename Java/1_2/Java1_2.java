33ade0c84caa71d7ccc124aa0c78d078d2e5e18d
liyang <YangKaw@126.com/KawYang@163.com>
2020-01-27 11:11:33 +0800

    创建文件夹

---
+++ Java/1_2/Java1_2.java
@@ -0,0 +1,9 @@
+public class Java1_2{
+    public static void main(String []args){
+        int x,y,sum;
+        x = 3;
+        y = 5;
+        sum = x + y;
+        System.out.println(sum);
+    }
+}
